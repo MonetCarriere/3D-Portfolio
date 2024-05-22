195c7976e0fc42ca8c2e7b9fd985ba6d1706c846
MonetCarriere <MonetCarriere@yahoo.com>
2024-05-22 05:26:09 -0700

    second commit. set up routes and created Nav pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-        </h1>
-    </div>
+   <main className="bg-slate-300/20">
+    <Router>
+       <Navbar/>
+        <Routes>
+            <Route path="/" element={'Home'}/>
+            <Route path="/about" element={'About'}/>
+            <Route path="/project" element={'Project'}/>
+            <Route path="/contact" element={'Contact'}/>
+        </Routes>
+    </Router>
+   </main>
   )
 }
 
