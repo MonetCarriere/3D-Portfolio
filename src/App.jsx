f0783d85248cbab47c50aeb1aa99538726d7554f
MonetCarriere <MonetCarriere@yahoo.com>
2024-06-06 09:32:48 -0700

    third commit   Rendered the Island

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {Home, About, Projects, Contact} from './pages';
 import Navbar from './components/Navbar';
 
 const App = () => {
@@ -8,10 +9,10 @@ const App = () => {
     <Router>
        <Navbar/>
         <Routes>
-            <Route path="/" element={'Home'}/>
-            <Route path="/about" element={'About'}/>
-            <Route path="/project" element={'Project'}/>
-            <Route path="/contact" element={'Contact'}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/project" element={<Projects/>}/>
+            <Route path="/contact" element={<Contact/>}/>
         </Routes>
     </Router>
    </main>
